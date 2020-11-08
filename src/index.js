00b8e87a094acf34f57b867da31f8d8a6fbc9dc5
Marina Domashenko <creator.marina@gmail.com>
2020-11-08 18:34:21 +0200

    Add task-timer

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import './css/style.css';
+
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.start();
+  }
+
+  start() {
+    const targetTime = this.targetDate.getDate();
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      const time = this.getTimeComponents(deltaTime);
+
+      this.updateCountdownTimer(time);
+      // console.log(`${days}:${hours}:${mins}:${secs}`);
+    }, 1000);
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+  updateCountdownTimer({ days, hours, mins, secs }) {
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.mins.textContent = `${mins}`;
+    refs.secs.textContent = `${secs}`;
+    //   const elem = document.getElementById('#timer-1');
+    //   elem.innerHTML = days + ':' + hours + ':' + mins + ':' + secs;
+    //   setTimeout('#timer-1', 1000);
+  }
+}
+
+const countdownTimer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jan 1, 2021'),
+});
